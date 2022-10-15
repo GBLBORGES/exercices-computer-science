3ea1cdc82eca6eb844fc32bad1f0628b308e76e7
GabrielBorges <igblborges@gmail.com>
2022-10-15 12:27:09 -0300

    feat: resolve problemas com logica que necessita de repetiaco ('for')

--- introduction-to-python/exercises/repetition.py
+++ introduction-to-python/exercises/repetition.py
@@ -1,10 +1,17 @@
 def append_each_letter_of_the_word_in_a_list(word):
-    return
+    list = []
+    for letter in word:
+        list.append(letter)
+    return list
 
 
 def return_index_of_the_uppercase_letter(word):
-    return
+    for letter in word:
+        if letter.isupper():
+            return word.index(letter)
 
 
 def return_element_from_list_that_is_string(input_list):
-    return
+    for element in input_list:
+        if isinstance(element, str):
+            return element
