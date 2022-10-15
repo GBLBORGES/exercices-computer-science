60b40ffac272ed5cab750497af3cd7b49b6c1e79
GabrielBorges <igblborges@gmail.com>
2022-10-15 11:31:54 -0300

    feat: resolvo problemas relacionados a tipos de dados e manipulacao dos mesmos

--- introduction-to-python/exercises/python_data_types.py
+++ introduction-to-python/exercises/python_data_types.py
@@ -1,58 +1,66 @@
+
+
 def is_bool(value):
-    return
+    return isinstance(value, bool)
 
 
 def is_int(value):
-    return
+    return isinstance(value, int)
 
 
 def is_float(value):
-    return
+    return isinstance(value, float)
 
 
 def is_string(value):
-    return
+    return isinstance(value, str)
 
 
 def is_list(value):
-    return
+    return isinstance(value, list)
 
 
 def is_tuple(value):
-    return
+    return isinstance(value, tuple)
 
 
 def is_set(value):
-    return
+    return isinstance(value, set)
 
 
 def is_dict(value):
-    return
+    return isinstance(value, dict)
 
 
-def return_lower_case_string(word):
-    return
+def return_lower_case_string(word: str):
+    return word.lower()
 
 
-def append_element_in_list(element, input_list):
-    return
+def append_element_in_list(element, input_list: list):
+    input_list.append(element)
+    return input_list
 
 
-def remove_element_from_list(element, input_list):
-    return
+def remove_element_from_list(element, input_list: list):
+    input_list.remove(element)
+    return input_list
 
 
-def create_new_key_value_in_dict(key, value, input_dict):
-    return
+def create_new_key_value_in_dict(key, value, input_dict: dict):
+    input_dict.update({key: value})
+    return input_dict
 
 
-def delete_key_from_dict(key, input_dict):
-    return
+def delete_key_from_dict(key, input_dict: dict):
+    input_dict.pop(key)
+    return input_dict
 
 
 def add_element_to_set(element, input_set):
-    return
+    input_set.add(element)
+    return input_set
 
 
 def remove_element_from_set(element, input_set):
-    return
+    input_set.remove(element)
+    return input_set
